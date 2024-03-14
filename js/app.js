b53abd78cacde137e0c2a0d6a7e68dcc21a0eb5d
Jamie Phillips <152644895+jamiephillips212@users.noreply.github.com>
2024-03-14 00:49:22 -0500

    re-wrote code

--- js/app.js
+++ js/app.js
@@ -1,134 +1,150 @@
 'use strict';
 
-let correctCount = 0;
+let score = 0;
 
-function getName() {
-  let usersName = prompt('Hey! What is your name?');
-  
-  while (usersName === null || usersName === '') {
-    if (usersName === null) {
-      usersName = confirm('Would you like to skip?') ? '' : prompt('Enter a name');
+const bioQuestions = [
+{
+  question: 'Is my boyfriend the reason I keep going?',
+  answer: 'yes',
+  congratulate: 'Yes. He is the reason I keep going!'
+  giveAnswer: 'No. He is the reason I continue.',
+  questionType: 'YN'
+},
+{
+  question: 'Is red my favorite color?'
+  answer: 'yes',
+  congratulate: "Red is my favorite color! Nice Job!",
+  giveAnswer: 'No, you guessed wrong. My favorite color is red.',
+  questionType: 'Y/N'
+},
+{
+  question: 'Am I from OKC, OK?',
+  answer: 'yes'
+  congratulate: 'Nice, You are correct!'
+  giveAnswer: 'No, Incorrect. I am from OKC.'
+  questionType: 'YN'
+},
+{
+ question: 'Do I like games?',
+ answer: 'yes',
+ congratulate: 'I love games, from PC to Console!',
+ giveAnswer: 'Who would not like games?'
+ questionType:'YN'
+},
+{
+  question: 'Is Keshi my favorite artist?',
+  answer: 'yes'
+  congratulate: 'Keshi is my favorite artist. Nice!'
+  giveAnswer: 'Wrong. Keshi is my favorite artist.'
+}
+{
+  question: 'I am thinking of a number between 1-10, try and guess.'
+  answer: Math.floor(Math.random()) * (10 - i * 1)
+  questionType: 'INT',
+  allowedAttempts: 4
+},
+{
+ question: 'Would I like to visit Japan in the future?',
+ answer: 'yes'
+ congratulate: 'Yeah. I would to love to visit.'
+ giveAnswer: 'I would like to visit.',
+}
+];
+
+function askQuestions(questions) {
+  for (const q of questions)
+  let answer;
+switch (q.questionType) {
+  case 'YN':
+}
+},
+    answer = sanitizeInput(promptuser(q.question), 'str');
+    while (answer === '' || answer === null || answer !== 'yes' && answer !== 'no'){
+      answer = sanitizeInput(promptuser(q.question + ' Valid input: yes/no or y/n'), 'str');
+    }
+    if (answer == q.answer) {
+      score++;
+      messageUser(q.congratulate);
     } else {
-      usersName = prompt('Enter a name');
+      messageUser (q.giveAnswer);
     }
-  }
-
-  return usersName;
-}
-
-let usersName = getName();
-
-alert(`Welcome, ${usersName}: Hit OK to begin the quiz!`);
-alert('I am going to ask you several Yes or No questions. Hit OK when you are ready to begin.');
-
-function getResponse() {
-  const metResponse = prompt('Is my boyfriend the reason I keep going?');
-  let lowercaseResponse = metResponse.toLowerCase();
-
-  if (lowercaseResponse === 'no') {
-    alert('Unfortunately, that is incorrect. Try again!');
-  } else if (lowercaseResponse === 'yes') {
-    alert('Correct! Great Job!');
-    correctCount += 1;
-  }
-}
-
-getResponse();
-
-function colorAnswer() {
-  const faveColor = prompt('Is red my favorite color?');
-  let lowercaseColor = faveColor.toLowerCase();
-
-  if (lowercaseColor === 'no') {
-    alert('Unfortunately, you guessed wrong! Red is my favorite color!');
-  } else if (lowercaseColor === 'yes') {
-    alert('Correct! Good job!');
-    correctCount += 1;
-  }
-}
-
-colorAnswer();
-
-function getTown() {
-  const homeTown = prompt('Am I from OKC, OK?');
-  let lowercaseTown = homeTown.toLowerCase();
-
-  if (lowercaseTown === 'no') {
-    alert('Incorrect! Try again.');
-  } else if (lowercaseTown === 'yes') {
-    alert('Wow! You got it right!');
-    correctCount += 1;
-  }
-}
-
-getTown();
-
-function games() {
-  const gamesResponse = prompt('Do I like games?');
-  let lowercaseGames = gamesResponse.toLowerCase();
-
-  if (lowercaseGames === 'no') {
-    alert('Incorrect. I love games!');
-  } else if (lowercaseGames === 'yes') {
-    alert('Correct! Nice Going!');
-    correctCount += 1;
-  }
-}
-
-games();
-
-function artist() {
-  const artistResponse = prompt('Is keshi my favorite artist?');
-  let lowercaseArtist = artistResponse.toLowerCase();
-
-  if (lowercaseArtist === 'no') {
-    alert('Wrong. Please try again.');
-  } else if (lowercaseArtist === 'yes') {
-    alert('Wonderful! That is correct!');
-    correctCount += 1;
-  }
-}
-
-artist();
-
-let myNumber = 29;
-
-function findNumber() {
-  const maxAttempts = 6;
-
-  for (let attempts = maxAttempts; attempts > 0; attempts--) {
-    let faveNumber = prompt('What is my favorite number? 1-100');
-    let replyNumber = parseInt(faveNumber);
-
-    if (isNaN(replyNumber)) {
-      alert('Please enter a valid number.');
-    } else if (replyNumber > myNumber) {
-      alert('Way too high! Try again!');
-    } else if (replyNumber < myNumber) {
-      alert('Way too low! Try again!');
-    } else if (replyNumber === myNumber) {
-      alert('Ding Ding Ding! You are correct! 29 is my favorite number. Congrats!');
-      correctCount += 1;
+    break;
+    case 'TF':
+      answer = promptuser (q.question);
       break;
-    }
+      case 'INT': {
+        let tracker = 0;
+        while(tracker <= q.allowedAttempts - 1) {
+          answer = parseInt(promptuser(q.question));
+          if (answer === q.answer) {
+            score++;
+            messageUser('You guessed the number in '+(tracker + 1) + ' tries!');
+            break;
+          } else if (tracker >= q.allowedAttempts - 1){
+            messageUser('Sorry. You did not get it. The number was ' + q.answer);
+            break;
+          } else {
+            if (answer < q.answer){
+              messageUser('The number is greater than ' +answer+ '.');
+            } else if (!Number,isInteger (answer) || answer < 10) {
+              messageUser('Use a number between 1-10');
+            }
+          }
+          tracker++;
+          }
+          break;
+          }
+        case 'MC': let gotIt = false;
+          let tries = q.allowedAttempts;
+          while (tries < 0 && gotIt !== true){
+            answer = promptuser(q.question);
+            if (answer === null)}
+            
 
-    if (attempts === 1) {
-      alert('You are out of attempts, my favorite number is 29.');
-    }
-  }
-}
-function trip() {
-  const tripResponse = prompt('Would I like to visit Japan in the future?');
-  let lowercasetrip = tripResponse.toLowerCase();
-
-  if (lowercasetrip === 'no') {
-    alert('Not correct. I would love to visit.');
-  } else if (lowercasetrip === 'yes') {
-    alert('Nice job. You are correct!');
-    correctCount += 1;
-  }
-}
-
-findNumber();
-
-alert(`Thanks for participating in this quiz, ${usersName}! You got ${correctCount} out of 6 correct!`);
+          answer = answer.toLowerCase();
+          for (const correctAnswer of q.question)
+            if (answer === correctAnswer.toLowerCase()){
+              score++;
+              messageUser(q.congratulate);
+              gotIt = true;
+            }
+              function sanitizeInput(inputValue, inputType) {
+                if (inputType === 'str'){
+                  if (inputValue === null){
+                    inputValue = '';
+                  }
+                  inputValue = inputValue.toLowerCase();
+                  if (inputValue === 'y'){
+                    inputValue = 'yes';
+                  } else if (inputValue === 'n') {
+                    inputValue = 'no';
+                  }
+                  return inputValue;
+                }
+              }
+              
+              function kickOff() {
+                let userName = getUserName();
+                messageUser('Hello,' + userName + ' welcome to my site!');
+              
+                if (userName !== 'cc'){
+                  askGenericQuestions(bioQuestions);
+                }
+              
+                if (userName !== 'cc'){
+                  messageUser('Thanks for participating in this quiz ' + userName + ' you scored' + score + '/' + bioQuestions.length + '!');
+                } else {
+                  messageUser('Cheaters never prosper!');
+                }
+              }
+              
+              function getUserName() {
+                let userName = prompt('What is your name?');
+                return userName;
+              }
+              
+              function messageUser(msg) {
+                alert(msg);
+              }
+              
+              kickOff();
